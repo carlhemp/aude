9c77dc064d188b79669a5951261330aef0b9f216
Carl Hempel <cshempel@gmail.com>
2021-02-19 17:10:01 -0500

    Create Tile class

---
+++ lib.js
@@ -0,0 +1,73 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class Tile {
+  constructor(top, right, bottom, left, center, background){
+    let tileTypes = ['grass', 'city', 'road', 'river', 'water'];
+    let centerTypes = ['abbey', 'garden', null, null];
+
+    function standardizeTile(tile){
+      return (tileTypes.includes(tile) ? tile : tileTypes[getRandomInt(4)]);
+    }
+
+    this.top = standardizeTile(top);
+    this.right = standardizeTile(right);
+    this.bottom = standardizeTile(bottom);
+    this.left = standardizeTile(left);
+    this.background = standardizeTile(background);
+    this.center = (centerTypes.includes(center) ? center : centerTypes[getRandomInt(4)]);
+    this.rotated = 0;
+    this.meeple = null;
+
+    return this;
+  }
+  // matchThis is a 4 item array of tile types ['grass', 'city', 'grass', 'wildcard']
+  // returns an array of possible rotations or false;
+  match(matchThis) {
+    let tileString = `'${this.top}','${this.right}','${this.bottom}','${this.left}','${this.top}','${this.right}','${this.bottom}'`;
+    let proposedMatchString = ("'"+matchThis.join("','")+"'").replace(/\'wildcard\'/g,'(\'[^,]+\')');
+
+    let re = new RegExp('(?=('+proposedMatchString+'))','g');
+    let result = Array.from(tileString.matchAll(re), x => x.index);
+    result = result.map(x => (x != 0 ? tileString.slice(0, x).match(/,/g).length : 0));
+
+    if(result.length > 0){
+      return result;
+    } else {
+      return false;
+    }
+  }
+  rotateTile(matchThis = false) {
+    if(matchThis){
+      let rotations = this.match(matchThis);
+      let rotate = 0;
+      if(rotations == false) {
+        return false;
+      }
+      if(rotations[0] == 0 && rotations.length > 1){
+        rotate = 1;
+      }
+      for(let i = 1; i <= rotations[rotate]; i++) {
+        this.rotateTile(); //rotate as much as needed
+      }
+      console.log(this.rotated);
+      return this; //return a success? or failure?
+    }
+    else {  //we rotate counter-clockwise
+      //keep the meeple connected to it's place.
+      let options = ['top','right','bottom','left'];
+      if(this.meeple && this.meeple != ('center' || 'background')){
+        this.meeple = options[(options.indexOf(this.meeple)+3)%4];
+      }
+      let tmp = this.top;
+      this.top = this.right;
+      this.right = this.bottom;
+      this.bottom = this.left;
+      this.left = tmp;
+      this.rotated = (1+this.rotated)%4; //rotate = 0 to 3
+
+      return this;
+    }
+  }
+}
